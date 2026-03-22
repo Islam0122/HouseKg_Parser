9cdcaf88b87558474bb54e748500355f36c63461
islam0122 <duishobaevislam01@gmail.com>
2026-03-22 15:25:06 +0600

    6-commit

--- deployment/server-entrypoint.sh
+++ deployment/server-entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 
-python manage.py collectstatic --noinput
+# Используем виртуальное окружение
+. .venv/bin/activate
 
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
+# Создание суперпользователя, если нет
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 
+# Запуск Gunicorn
 exec gunicorn project.wsgi:application \
     --bind 0.0.0.0:${PORT:-8000} \
     --workers 3 \
