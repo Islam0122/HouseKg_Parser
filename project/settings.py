779de417309bc58d640ed05f7e937befc26f6f89
islam0122 <duishobaevislam01@gmail.com>
2026-03-22 16:16:39 +0600

    11

--- project/settings.py
+++ project/settings.py
@@ -67,9 +67,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("PGHOST", default="db"),
+        "USER": os.getenv("PGUSER", default="postgres"),
+        "NAME": os.getenv("PGDATABASE", default="postgres"),
+        "PASSWORD": os.getenv("PGPASSWORD", default="postgres"),
+        "PORT": os.getenv("PGPORT", default=5432),
+        "CONN_MAX_AGE": 60,
     }
 }
 
